471adef206802e14ae6d2670f974c42986d7e130
geetanjaligg <anjaligg@gmail.com>
2015-02-03 19:06:07 +0530

    add test.log source link

--- src/main/scala/count.scala
+++ src/main/scala/count.scala
@@ -6,7 +6,7 @@ object count{
     val sparkconf = new SparkConf().setMaster("local").setAppName("count")
     val sc = new SparkContext(sparkconf)
 
-    val file = sc.textFile("src/resources/test.log")
+    val file = sc.textFile("src/resources/test.log") // source : http://www.monitorware.com/en/logsamples/apache.php
     println("Count: " + file.count())
 
   }
